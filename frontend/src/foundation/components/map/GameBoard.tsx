f0b95573f2b31bd28e502c19a4bbcabc694742a9
Romeo Sofia <romeo.sofia@epfl.ch>
2026-09-15 11:15:38 +0200

    fix: no border

--- frontend/src/foundation/components/map/GameBoard.tsx
+++ frontend/src/foundation/components/map/GameBoard.tsx
@@ -21,7 +21,9 @@ export function GameBoard({ layoutData = [] }: GameBoardProps) {
   }
 
   return (
-    <div className="grid grid-cols-[repeat(10,50px)] gap-[2px] w-max bg-neutral-800 p-1 rounded-md">      {tiles.map((tile) => (
+    <div className="grid grid-cols-[repeat(10,50px)] gap-[2px] w-max bg-neutral-800 p-1 rounded-md">
+      {' '}
+      {tiles.map((tile) => (
         <Tile key={`game-${tile.x}-${tile.y}`} tile={tile} />
       ))}
     </div>
